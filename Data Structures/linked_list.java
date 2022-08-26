e8548e8b14c430e93e2c69e4547980a4a984a27c
Withanage Perera <78891315+WithanagePerera@users.noreply.github.com>
2022-08-26 09:40:15 -0400

    Comments for linked list.

---
+++ Data Structures/linked_list.java
@@ -0,0 +1,5 @@
+// Elements are not stored in contiguous locations
+// Elements are linked using pointers
+// Each element is called a node
+// Nodes cannot be accessed directly instead
+// Must start from head and follow through link to reach desired node
