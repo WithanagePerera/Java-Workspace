e23cb85eab79701ca1479fc0cfdc150e94e8dcd9
Withanage Perera <78891315+WithanagePerera@users.noreply.github.com>
2022-08-26 09:56:37 -0400

    LinkedList complete.

--- Data Structures/linked_list.java
+++ Data Structures/linked_list.java
@@ -3,3 +3,25 @@
 // Each element is called a node
 // Nodes cannot be accessed directly instead
 // Must start from head and follow through link to reach desired node
+
+public class linked_list
+{
+
+    import java.util.LinkedList;
+
+    public static void main (String [] args)
+    {
+        LinkedList<Integer> example = new LinkedList<>();
+
+        for (int i = 0; i < 30; i++)
+        {
+            example.add(i);
+        }
+
+        for (Integer pass : example)
+            System.out.print(pass + " ");
+
+        System.out.println("Element at index 5: " example.get(5));
+        System.out.println("Element at index 10: " example.get(10));
+    }
+}
