7823811fe8848ad5df9d3282223f2c1c995df336
Withanage Perera <78891315+WithanagePerera@users.noreply.github.com>
2022-08-30 09:58:48 -0400

    Updates to Queue implementations.

--- Data Structures/queue.java
+++ Data Structures/queue.java
@@ -2,8 +2,66 @@
 // Elements are inserted at the end of the list
 // Elements are deleted from the beginning of the list
 
-public class Queue
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class importQueue
 {
-    int front, rear, size, cap;
-    int arr[];
+    public static void main (String [] args)
+    {
+        Queue<Integer> example = new LinkedList();
+        for (int i = 0; i < 25; i++)
+        {
+            example.add(i);
+        }
+
+        System.out.println("Elements of imported Queue:");
+
+        for (int pass : example)
+        {
+            System.out.print(pass + " ");
+        }
+
+        for (int i = 0; i < 5; i++)
+        {
+            example.remove();
+        }
+
+        System.out.println("\nElements of imported Queue after 5 deletions:");
+
+        for (int pass : example)
+        {
+            System.out.print(pass + " ");
+        }
+    }
+}
+
+public class initializeQueue
+{
+
+    public class Queue
+    {
+        int front, rear, size, capacity;
+        int arr[];
+
+        public Queue (int size)
+        {
+            capacity = size;
+            front = 0;
+            size = 0;
+
+            rear = capacity -1;
+            arr = new int[queue.capacity];
+        }
+    }
+
+    public static void main (String [] args)
+    {
+        Queue example = new Queue(5);
+
+        for (int i = 0; i < example.capacity; i++)
+        {
+            System.out.println(example.arr[i]);
+        }
+    }
 }
