292bf9ea1914870b71cb13ef0b30a1e1547d61f1
Withanage Perera <78891315+WithanagePerera@users.noreply.github.com>
2022-08-30 14:09:08 -0400

    Dijkstra

---
+++ Algorithms/Dijkstra.java
@@ -0,0 +1,11 @@
+// Generates a shortest path
+// Maintains 2 sets:
+// 1 set contains verticies includde in the shortest path
+// 1 set contains verticies not yet included/checked
+// Assign the verticies values representing the number of steps from the origin
+// This node value updates as you try each path
+// Ultimately stops when you've reached the smallest node value
+
+public class Dijkstra
+{
+}
